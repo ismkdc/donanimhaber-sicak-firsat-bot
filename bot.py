9e42fb182c1109287f1aea5571b6e799b7c0d225
ismail kundakcı <ism.kundakci@hotmail.com>
2020-04-26 02:45:31 +0300

    writed program for getting hots from dh

---
+++ bot.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import telegram
+
+r = requests.get("https://forum.donanimhaber.com/api2/GlobalApi/GetMorePopularSicakFirsatTopics?type=2&skip=0")
+html = r.json()["Data"]["Html"]
+
+source = BeautifulSoup(html, "lxml")
+topics = source.find_all("a", attrs={"class":"satirikapla"})
+result_str = ""
+for topic in topics:
+    title = topic.findChildren("div" , recursive=False)[0].text
+    result_str += f'{title}: https://forum.donanimhaber.com{topic["href"]}\n\n'
+
+bot = telegram.Bot("bot_key")
+bot.send_message(chat_id="chat_id", text=result_str)
